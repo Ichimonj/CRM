35510d0b70c260eb363e32e7fbb82dbea191622a
Oleg <susheffol@icloud.com>
2025-12-15 06:31:06 +0300

    Update address

--- DataModels/Address/address.cpp
+++ DataModels/Address/address.cpp
@@ -4,59 +4,111 @@ Address::Address(
     const std::string& country,
     const std::string& city,
     const std::string& street,
-    const std::string& house,
+    const std::string& house_number,
     const std::string& apartment,
     const std::string& postal_code
 )
     : country(country)
     , city(city)
     , street(street)
-    , house(house)
+    , house_number(house_number)
     , apartment(apartment)
     , postal_code(postal_code)
+    , is_validated(false)
 {
 }
 
 Address::Address(const Address& address)
     : country(address.country)
+    , country_code(address.country_code)
+    , region(address.region)
+    , region_code(address.region_code)
     , city(address.city)
+    , district(address.district)
+    , dependent_locality(address.dependent_locality)
     , street(address.street)
-    , house(address.house)
+    , house_number(address.house_number)
+    , building(address.building)
     , apartment(address.apartment)
+    , floor(address.floor)
+    , entrance(address.entrance)
     , postal_code(address.postal_code)
+    , po_box(address.po_box)
+    , landmark(address.landmark)
+    , directions(address.directions)
+    , formatted_address(address.formatted_address)
+    , latitude(address.latitude)
+    , longitude(address.longitude)
+    , altitude(address.altitude)
+    , is_validated(address.is_validated)
+    , standard_format(address.standard_format)
 {
 }
 
 bool Address::operator==(const Address& other) const
 {
-    if (this->country != other.country) return false;
-    if (this->city != other.city) return false;
-    if (this->street != other.street) return false;
-    if (this->apartment != other.apartment) return false;
-    if (this->postal_code != other.postal_code) return false;
-    return true;
+    return country == other.country && country_code == other.country_code &&
+           region == other.region && region_code == other.region_code && city == other.city &&
+           district == other.district && dependent_locality == other.dependent_locality &&
+           street == other.street && house_number == other.house_number &&
+           building == other.building && apartment == other.apartment && floor == other.floor &&
+           entrance == other.entrance && postal_code == other.postal_code &&
+           po_box == other.po_box && landmark == other.landmark && directions == other.directions &&
+           formatted_address == other.formatted_address &&
+           standard_format == other.standard_format && latitude == other.latitude &&
+           longitude == other.longitude && altitude == other.altitude &&
+           is_validated == other.is_validated;
 }
 
 bool Address::operator!=(const Address& other) const
 {
-    if (this->country != other.country) return true;
-    if (this->city != other.city) return true;
-    if (this->street != other.street) return true;
-    if (this->apartment != other.apartment) return true;
-    if (this->postal_code != other.postal_code) return true;
-    return false;
+    return !(*this == other);
 }
 
-void Address::setCountry(const std::string& country) { this->country = country; }
-void Address::setCity(const std::string& city) { this->city = city; }
-void Address::setStreet(const std::string& street) { this->street = street; }
-void Address::setHouse(const std::string& house) { this->house = house; }
-void Address::setApartment(const std::string& apartment) { this->apartment = apartment; }
-void Address::setPostalCode(const std::string& postal_code) { this->postal_code = postal_code; }
+auto Address::getCountry() const -> const std::string& { return country; }
+auto Address::getCountryCode() const -> const std::string& { return country_code; }
+auto Address::getRegion() const -> const std::string& { return region; }
+auto Address::getRegionCode() const -> const std::string& { return region_code; }
+auto Address::getCity() const -> const std::string& { return city; }
+auto Address::getDistrict() const -> const std::string& { return district; }
+auto Address::getDependentLocality() const -> const std::string& { return dependent_locality; }
+auto Address::getStreet() const -> const std::string& { return street; }
+auto Address::getHouse() const -> const std::string& { return house_number; }
+auto Address::getBuilding() const -> const std::string& { return building; }
+auto Address::getApartment() const -> const std::string& { return apartment; }
+auto Address::getFloor() const -> const std::string& { return floor; }
+auto Address::getEntrance() const -> const std::string& { return entrance; }
+auto Address::getPostalCode() const -> const std::string& { return postal_code; }
+auto Address::getPoBox() const -> const std::string& { return po_box; }
+auto Address::getLandmark() const -> const std::string& { return landmark; }
+auto Address::getDirections() const -> const std::string& { return directions; }
+auto Address::getFormattedAddress() const -> const std::string& { return formatted_address; }
+auto Address::getStandardFormat() const -> const std::string& { return standard_format; }
+auto Address::getLatitude() const -> const std::optional<double> { return latitude; }
+auto Address::getLongitude() const -> const std::optional<double> { return longitude; }
+auto Address::getAltitude() const -> const std::optional<double> { return altitude; }
+auto Address::getIsValidated() const -> const bool { return is_validated; }
 
-const std::string& Address::getCountry() const { return this->country; }
-const std::string& Address::getCity() const { return this->city; }
-const std::string& Address::getStreet() const { return this->street; }
-const std::string& Address::getHouse() const { return this->house; }
-const std::string& Address::getApartment() const { return this->apartment; }
-const std::string& Address::getPostalCode() const { return this->postal_code; }
+void Address::setCountry(const std::string& value) { country = value; }
+void Address::setCountryCode(const std::string& value) { country_code = value; }
+void Address::setRegion(const std::string& value) { region = value; }
+void Address::setRegionCode(const std::string& value) { region_code = value; }
+void Address::setCity(const std::string& value) { city = value; }
+void Address::setDistrict(const std::string& value) { district = value; }
+void Address::setDependentLocality(const std::string& value) { dependent_locality = value; }
+void Address::setStreet(const std::string& value) { street = value; }
+void Address::setHouseNumber(const std::string& value) { house_number = value; }
+void Address::setBuilding(const std::string& value) { building = value; }
+void Address::setApartment(const std::string& value) { apartment = value; }
+void Address::setFloor(const std::string& value) { floor = value; }
+void Address::setEntrance(const std::string& value) { entrance = value; }
+void Address::setPostalCode(const std::string& value) { postal_code = value; }
+void Address::setPoBox(const std::string& value) { po_box = value; }
+void Address::setLandmark(const std::string& value) { landmark = value; }
+void Address::setDirections(const std::string& value) { directions = value; }
+void Address::setFormattedAddress(const std::string& value) { formatted_address = value; }
+void Address::setStandardFormat(const std::string& value) { standard_format = value; }
+void Address::setLatitude(std::optional<double> value) { latitude = value; }
+void Address::setLongitude(std::optional<double> value) { longitude = value; }
+void Address::setAltitude(std::optional<double> value) { altitude = value; }
+void Address::setIsValidated(bool value) { is_validated = value; }
--- DataModels/Address/address.hpp
+++ DataModels/Address/address.hpp
@@ -1,6 +1,6 @@
 #pragma once
+#include <optional>
 #include <string>
-
 /// @brief Storage structure of the physical address
 struct Address {
     Address() = default;
@@ -8,40 +8,104 @@ struct Address {
         const std::string& country,
         const std::string& city,
         const std::string& street,
-        const std::string& house,
+        const std::string& house_number,
         const std::string& apartment,
         const std::string& postal_code
     );
     Address(const Address& address);
+
     bool operator==(const Address& other) const;
     bool operator!=(const Address& other) const;
 
 public:
     /// @name Getters
     /// @{
-    const std::string& getCountry() const;
-    const std::string& getCity() const;
-    const std::string& getStreet() const;
-    const std::string& getHouse() const;
-    const std::string& getApartment() const;
-    const std::string& getPostalCode() const;
+    auto getCountry() const -> const std::string&;
+    auto getCountryCode() const -> const std::string&;
+    auto getRegion() const -> const std::string&;
+    auto getRegionCode() const -> const std::string&;
+    auto getCity() const -> const std::string&;
+    auto getDistrict() const -> const std::string&;
+    auto getDependentLocality() const -> const std::string&;
+    auto getStreet() const -> const std::string&;
+    auto getHouse() const -> const std::string&;
+    auto getBuilding() const -> const std::string&;
+    auto getApartment() const -> const std::string&;
+    auto getFloor() const -> const std::string&;
+    auto getEntrance() const -> const std::string&;
+    auto getPostalCode() const -> const std::string&;
+    auto getPoBox() const -> const std::string&;
+    auto getLandmark() const -> const std::string&;
+    auto getDirections() const -> const std::string&;
+    auto getFormattedAddress() const -> const std::string&;
+    auto getLatitude() const -> const std::optional<double>;
+    auto getLongitude() const -> const std::optional<double>;
+    auto getAltitude() const -> const std::optional<double>;
+    auto getIsValidated() const -> const bool;
+    auto getStandardFormat() const -> const std::string&;
     /// @}
 
     /// @name Setters
     /// @{
-    void setCountry(const std::string& country);
-    void setCity(const std::string& city);
-    void setStreet(const std::string& street);
-    void setHouse(const std::string& house);
-    void setApartment(const std::string& apartment);
-    void setPostalCode(const std::string& postal_code);
+    void setCountry(const std::string& value);
+    void setCountryCode(const std::string& value);
+    void setRegion(const std::string& value);
+    void setRegionCode(const std::string& value);
+    void setCity(const std::string& value);
+    void setDistrict(const std::string& value);
+    void setDependentLocality(const std::string& value);
+    void setStreet(const std::string& value);
+    void setHouseNumber(const std::string& value);
+    void setBuilding(const std::string& value);
+    void setApartment(const std::string& value);
+    void setFloor(const std::string& value);
+    void setEntrance(const std::string& value);
+    void setPostalCode(const std::string& value);
+    void setPoBox(const std::string& value);
+    void setLandmark(const std::string& value);
+    void setDirections(const std::string& value);
+    void setFormattedAddress(const std::string& value);
+    void setStandardFormat(const std::string& value);
+    void setLatitude(std::optional<double> value);
+    void setLongitude(std::optional<double> value);
+    void setAltitude(std::optional<double> value);
+    void setIsValidated(bool value);
     /// @}
 
 private:
-    std::string country     = "";
-    std::string city        = "";
-    std::string street      = "";
-    std::string house       = "";
-    std::string apartment   = "";
-    std::string postal_code = "";
+    // Country
+    std::string country;       // "United States"
+    std::string country_code;  // "US" (ISO 3166-1 alpha-2)
+
+    // Region, state
+    std::string region;
+    std::string region_code;
+
+    // City
+    std::string city;
+    std::string district;  // City area
+    std::string dependent_locality;
+
+    // Street, house
+    std::string street;
+    std::string house_number;
+    std::string building;   // "Tower A", "Block B"
+    std::string apartment;  // "Apt 4B", "Suite 500"
+    std::string floor;      // "5th Floor"
+    std::string entrance;   // "Entrance 2"
+
+    std::string postal_code;  // "90028" "SW1A 1AA"
+    std::string po_box;       // "PO Box 123"
+
+    std::string landmark;    // "Near Central Park"
+    std::string directions;  // "3rd house on the left"
+    std::string formatted_address;
+
+    //
+    std::optional<double> latitude  = 0.0;
+    std::optional<double> longitude = 0.0;
+    std::optional<double> altitude  = 0.0;
+
+    bool                  is_validated;
+    std::string           standard_format;
 };
