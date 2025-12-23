9a420c2d6a88ad3471353f4912c4e52e613eab8b
Oleg <susheffol@icloud.com>
2025-12-23 12:08:38 +0300

    Add events logger

---
+++ Logger/events_log.cpp
@@ -0,0 +1,48 @@
+#include "events_log.hpp"
+
+#include "date.hpp"
+
+void EventLog::set_file(const std::string& file_name)
+{
+    std::lock_guard<std::mutex> lock(mt);
+    this->file = std::make_unique<std::ofstream>(file_name, std::ios::app);
+}
+
+void EventLog::log(
+    const LOG_LEVEL    level,
+    const std::string& message_file,
+    const std::string& line,
+    const std::string& message
+)
+{
+    if (file->is_open()) {
+        Date        date;
+        std::string result;
+        switch (level) {
+            case LOG_LEVEL::INFO:
+                result = "[INFO]:    ";
+                break;
+            case LOG_LEVEL::WARNING:
+                result = "[WARNING]: ";
+                break;
+            case LOG_LEVEL::ERROR:
+                result = "[ERROR]:   ";
+                break;
+            default:
+                result = "[DEFAULT]: ";
+        }
+        result += date.getDateStr() + " - ";
+
+        size_t start_file_name = message_file.find("Crm");
+        result += message_file.substr(start_file_name) + ":" + line + " - ";
+        result += message + '\n';
+        std::lock_guard<std::mutex> lock(mt);
+        file->write(result.c_str(), result.size());
+    }
+}
+
+EventLog& EventLog::getInstance()
+{
+    static EventLog instance;
+    return instance;
+}
---
+++ Logger/events_log.hpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <memory>
+#include <mutex>
+enum class LOG_LEVEL { INFO, WARNING, ERROR };
+class EventLog {
+public:
+    EventLog()                           = default;
+    EventLog(const EventLog&)            = delete;
+    EventLog& operator=(const EventLog&) = delete;
+
+    /// @name functions
+    /// @{
+    void set_file(const std::string& file_name);
+    void log(
+        const LOG_LEVEL    level,
+        const std::string& message_file,
+        const std::string& line,
+        const std::string& message
+    );
+    static EventLog& getInstance();
+    ///@}
+
+private:
+    std::mutex                     mt;
+    static EventLog*               instance;
+    std::unique_ptr<std::ofstream> file;
+};
