23cfd5d154154d1f7e48fe0738f9ebf587710203
Oleg <susheffol@icloud.com>
2026-01-01 22:54:00 +0300

    separated include test files

---
+++ Tests/tests_headers.hpp
@@ -0,0 +1,12 @@
+
+#include "Tests/BigNum/big_uint_tests.hpp"
+#include "Tests/BigNum/money_tests.hpp"
+#include "Tests/ChangeLog/chagne_log_tests.hpp"
+#include "Tests/ChangeLog/enums_to_str_tests.hpp"
+#include "Tests/DateTime/date_tests.hpp"
+#include "Tests/DateTime/time_duration_tests.hpp"
+#include "Tests/PhoneNumber/phone_number_tests.hpp"
+#include "Tests/Person/person_tests.hpp"
+#include "Tests/Person/client_tests.hpp"
+#include "Tests/Person/internal_employee_tests.hpp"
+#include "Tests/Person/external_employee_tests.hpp"
--- main.cpp
+++ main.cpp
@@ -1,15 +1,4 @@
-
-#include "Tests/BigNum/big_uint_tests.hpp"
-#include "Tests/BigNum/money_tests.hpp"
-#include "Tests/ChangeLog/chagne_log_tests.hpp"
-#include "Tests/ChangeLog/enums_to_str_tests.hpp"
-#include "Tests/DateTime/date_tests.hpp"
-#include "Tests/DateTime/time_duration_tests.hpp"
-#include "Tests/PhoneNumber/phone_number_tests.hpp"
-#include "Tests/Person/person_tests.hpp"
-#include "Tests/Person/client_tests.hpp"
-#include "Tests/Person/internal_employee_tests.hpp"
-#include "Tests/Person/external_employee_tests.hpp"
+#include "tests_headers.hpp"
 int main(int argc, char**argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
