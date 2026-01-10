72443bda1579ccdab73dd30e1303c90d4295fd26
Oleg <susheffol@icloud.com>
2026-01-10 19:01:54 +0300

    add TaskDataBase

---
+++ DataBase/TaskDataBase/task_db.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <map>
+#include <unordered_map>
+
+#include "Task/task.hpp"
+#include "Usings/type_usings.hpp"
+
+class TaskDataBase {
+public:
+private:
+};
--- DataBase/TenantContext/tenant_context.hpp
+++ DataBase/TenantContext/tenant_context.hpp
@@ -2,9 +2,11 @@
 #include "DataBase/PersonDataBase/client_db.hpp"
 #include "DataBase/PersonDataBase/external_employee_db.hpp"
 #include "DataBase/PersonDataBase/internal_employee_db.hpp"
+#include "DataBase/TaskDataBase/task_db.hpp"
 struct TenantContext {
     ClientDataBase&           client_data_base;
     ExternalEmployeeDataBase& external_employee_data_base;
     InternalEmployeeDataBase& internal_employee_data_base;
     DealDataBase&             deal_data_base;
+    TaskDataBase&             task_data_base;
 };
